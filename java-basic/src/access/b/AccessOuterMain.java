57ecf807956901bd8ae87dca3aa456015eff69d3
hyejinki <zlagg23@gmail.com>
2023-12-18 23:55:25 +0900

    접근 제어자1

---
+++ java-basic/src/access/b/AccessOuterMain.java
@@ -0,0 +1,22 @@
+package access.b;
+
+import access.a.AccessData;
+
+public class AccessOuterMain {
+    public static void main(String[] args) {
+        AccessData accessData = new AccessData();
+        // public 호출 가능
+        accessData.publicField = 1;
+        accessData.publicMethod();
+
+        //같은 패키지  default 호출 가능
+//        accessData.defaultField = 2;
+//        accessData.defaultMethod();
+
+//        private 호출 불가
+//        accessData.privateField = 3;
+//        accessData.privateMethod();
+
+        accessData.innerAccess();
+    }
+}
